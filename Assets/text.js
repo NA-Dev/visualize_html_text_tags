7bac6902b5c390579c0df28739e8165f2ea10d3b
NA-Dev <n.aaron.na@gmail.com>
2017-11-08 21:16:12 -0500

    added CSS style to text-style.html

--- Assets/text.js
+++ Assets/text.js
@@ -11,10 +11,10 @@ var text = "<h1>Heading 1</h1>\
         <h5>Heading 5</h5>\
         <h6>Heading 6</h6>\
         <hr>\
-        <p>\
+        <p id=\"p1\">\
             This is a paragraph. It has automatic margin at the top. Pariatur laborum occaecat eu ut incididunt dolore reprehenderit exercitation adipisicing magna ut fugiat ut commodo qui.\
         </p>\
-        <p>\
+        <p id=\"p2\">\
             This is another paragraph. It has top margin too. Sit cupidatat aliquip commodo sed occaecat minim consectetur fugiat elit excepteur amet minim do ut adipisicing.</p>\
         <hr>\
         <pre>\
@@ -26,44 +26,43 @@ This <br>\
         </pre>\
         <hr>\
         <address>\
-            Address tag does this:<br>\
             Written by <a href=\"mailto:webmaster@example.com\">Nikki Aaron</a><br>\
-            3000 Mulberry Lane<br>\
-            Jamestown, VA 25978\
+            3000 Nottareal Place<br>\
+            Jamestown, VA 22222\
         </address>\
         <hr>\
-        <p>\
+        <p id=\"presentation\">\
             Text can be styled for presentation as...\
             <br>\
             <br>\
             <b>bold</b>, <i>italics</i>, <u>underline</u>, <s>incorrect</s>, <ins>inserted</ins>, <del>deleted</del>, <tt>teletype</tt>, <sup>superscript</sup>, <sub>subscript</sub>, <strike>strikethrough</strike>, <big>big</big>, or <small>small</small>.\
         </p>\
         <hr>\
-        <p>\
+        <p id=\"semantics\">\
             Phrases can be tagged semantically, sometimes that adds style...\
-            </br>\
-            </br>\
-             <strong>strong</strong>, <em>emphasized</em>, <dfn>definition</dfn>, <cite>citation</cite>, <var>variable</var>, <q>quote</q>, <code>code</code>, <kbd>keyboard input</kbd>, <samp>sample</samp>, <blockquote>- \"blockquote\"</blockquote>\
+            <br>\
+            <br>\
+             <strong>strong</strong>, <em>emphasized</em>, <dfn>definition</dfn>, <cite>citation</cite>, <var>variable</var>, <q>quote</q>, <code>code</code>, <kbd>keyboard input</kbd>, <samp>sample</samp>, \
             <br>\
             sometimes that does not add style...\
-            </br>\
-            </br>\
+            <br>\
+            <br>\
             <abbr>abbr.</abbr>, <acronym>acronym</acronym>\
         </p>\
         <hr>\
-        <p><ul>You Can Make Lists\
+        <p id=\"ul\"><ul>You Can Make Lists\
             <li>Of</li>\
             <li>Unordered</li>\
             <li>Things</li>\
         </ul></p>\
         <hr>\
-        <p><ol>You Can Make Lists\
+        <p id=\"ol\"><ol>You Can Make Lists\
             <li>Of</li>\
             <li>Ordered</li>\
             <li>Things</li>\
         </ol></p>\
         <hr>\
-        <p><dl>You Can Make Lists Of\
+        <p id=\"dl\"><dl>You Can Make Lists Of\
             <dt>Things</dt>\
             <dd>-with descriptions</dd>\
             <dt>And other things</dt>\
